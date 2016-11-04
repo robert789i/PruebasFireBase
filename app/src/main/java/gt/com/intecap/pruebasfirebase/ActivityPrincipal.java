59492d669a94e3dd393d342fba074e005393bd1e
robert789i <robertoramos789@gmail.com>
2016-11-04 11:20:47 -0600

    Initial commit

---
+++ app/src/main/java/gt/com/intecap/pruebasfirebase/ActivityPrincipal.java
@@ -0,0 +1,13 @@
+package gt.com.intecap.pruebasfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
